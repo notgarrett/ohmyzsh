47ec413d97c61b8a39c60a17181e60aabd03f1a3
notgarrett <garrettrgiles@gmail.com>
2023-04-28 10:58:54 -0400

    test

--- custom/my-aliases.zsh
+++ custom/my-aliases.zsh
@@ -5,3 +5,4 @@ alias zshalias="nvim ~/.oh-my-zsh/custom/my-aliases.zsh"
 alias vi="nvim"
 alias vim="nvim"
 alias sshdesktop="ssh garrettg@192.168.250.89 -p 22"
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
