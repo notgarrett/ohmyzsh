d8504471f0b0bcf4f1883b72793a07bf8378e912
notgarrett <garrettrgiles@gmail.com>
2023-06-01 14:54:41 -0400

    push this up

--- custom/my-aliases.zsh
+++ custom/my-aliases.zsh
@@ -1,4 +1,4 @@
-alias ls="exa --all --icons --grid"
+alias ls="exa --all --icons --group-directories-first"
 alias zshconfig="nvim ~/.zshrc"
 alias cat="bat"
 alias zshalias="nvim ~/.oh-my-zsh/custom/my-aliases.zsh"
@@ -6,3 +6,4 @@ alias vi="nvim"
 alias vim="nvim"
 alias sshdesktop="ssh garrettg@192.168.250.89 -p 22"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias nvimconfig="nvim ~/.config/nvim"
