cc4534e941c80443069961f92eca009d38d61042
Garrett Giles <garrettrgiles@gmail.com>
2023-11-02 11:51:37 -0400

    conda activator

--- custom/my-aliases.zsh
+++ custom/my-aliases.zsh
@@ -7,3 +7,4 @@ alias vim="nvim"
 alias sshdesktop="ssh garrettg@192.168.250.89 -p 22"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias nvimconfig="nvim ~/.config/nvim"
+alias activateconda = 'source /opt/anaconda/bin/activate root'
