f2a320339d3725ba2f35bbb5a317d8e3d711c007
notgarrett <garrettrgiles@gmail.com>
2024-03-31 23:57:37 -0400

    Changes

--- custom/my-aliases.zsh
+++ custom/my-aliases.zsh
@@ -5,6 +5,6 @@ alias zshalias="nvim ~/.oh-my-zsh/custom/my-aliases.zsh"
 alias vi="nvim"
 alias vim="nvim"
 alias sshdesktop="ssh garrettg@192.168.250.89 -p 22"
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias config='$HOME/.nix-profile/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias nvimconfig="nvim ~/.config/nvim"
-alias activateconda = 'source /opt/anaconda/bin/activate root'
+alias activateconda='source /opt/anaconda/bin/activate root'
